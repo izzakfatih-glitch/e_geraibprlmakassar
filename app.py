ec2c08183ddeaada7b9ced17848d2be497bad3e4
izzakfatih-glitch <izzakfatih@gmail.com>
2026-08-02 13:32:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -373,7 +373,7 @@ UPLOAD_HTML = """<!DOCTYPE html>
       </div>
 
       <div class="gen-btn">
-        <button type="submit">""" + ICONS["bolt"] + """ Generate &amp; Gabungkan Dokumen Word</button>
+        <button type="submit">""" + ICONS["bolt"] + """ Generate &amp; Preview Dokumen Proposal</button>
         <div class="gen-note">Sistem akan memproses dan membuat dokumen Word final secara otomatis</div>
         <div class="spinner" id="spinner">\u23F3 Memproses dokumen, mohon tunggu...</div>
       </div>
